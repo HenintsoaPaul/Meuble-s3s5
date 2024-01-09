c9a406518d8e3e86886794595f0515ad76e9eeb3
HenintsoaPaul <henintsoapaul@gmail.com>
2024-01-09 10:03:50 +0300

    Initiation du repo du projet Meuble.

---
+++ src/main/java/jdbc/Categorie.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+/**
+ *
+ * @author tsoa
+ */
+@Entity
+@Table(name = "categorie", catalog = "meuble", schema = "public")
+@NamedQueries({
+    @NamedQuery(name = "Categorie.findAll", query = "SELECT c FROM Categorie c"),
+    @NamedQuery(name = "Categorie.findByIdcategorie", query = "SELECT c FROM Categorie c WHERE c.idcategorie = :idcategorie"),
+    @NamedQuery(name = "Categorie.findByNomcategorie", query = "SELECT c FROM Categorie c WHERE c.nomcategorie = :nomcategorie")})
+public class Categorie implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idcategorie", nullable = false)
+    private Integer idcategorie;
+    
+    @Column(name = "nomcategorie", length = 20)
+    private String nomcategorie;
+
+    public Categorie() {
+    }
+
+    public Categorie(Integer idcategorie) {
+        this.idcategorie = idcategorie;
+    }
+
+    public Integer getIdcategorie() {
+        return idcategorie;
+    }
+
+    public void setIdcategorie(Integer idcategorie) {
+        this.idcategorie = idcategorie;
+    }
+
+    public String getNomcategorie() {
+        return nomcategorie;
+    }
+
+    public void setNomcategorie(String nomcategorie) {
+        this.nomcategorie = nomcategorie;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idcategorie != null ? idcategorie.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Categorie)) {
+            return false;
+        }
+        Categorie other = (Categorie) object;
+        if ((this.idcategorie == null && other.idcategorie != null) 
+                || (this.idcategorie != null && !this.idcategorie.equals(other.idcategorie))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "jdbc.Categorie[ idcategorie=" + idcategorie + " ]";
+    }
+    
+}
